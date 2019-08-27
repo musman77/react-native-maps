391e31546f22e74ec0e7516d9338e070fef4f039
musman <musman@digital-dividend.com>
2019-08-27 13:10:58 +0500

    Just for testing

--- lib/android/src/main/java/com/airbnb/android/react/maps/osmdroid/OsmMapAnimationMarker.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/osmdroid/OsmMapAnimationMarker.java
@@ -27,6 +27,7 @@ import com.bumptech.glide.gifdecoder.StandardGifDecoder;
 import org.osmdroid.views.MapView;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.widget.Toast;
 
 import org.osmdroid.views.MapView;
 
@@ -85,7 +86,7 @@ public class OsmMapAnimationMarker extends  OsmMapMarker {
                         iconBitmapDrawable = bd;
                         if(marker != null)
                         marker.setIcon(bd);
-
+                        Toast.makeText(mContext,"Hello Javatpoint",Toast.LENGTH_SHORT).show();
                         mapView.invalidate();
                     }
 
