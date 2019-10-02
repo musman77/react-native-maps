07fca235efc45b6cfb1061ce7bdffea212e525ca
musman <musman@digital-dividend.com>
2019-10-02 16:51:49 +0500

    revert work

--- lib/android/src/main/java/com/airbnb/android/react/maps/osmdroid/OsmMapView.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/osmdroid/OsmMapView.java
@@ -126,11 +126,11 @@ public class OsmMapView extends MapView implements MapView.OnFirstLayoutListener
 //        //scrollTo((int)(getMapScrollX()), (int)(getMapScrollY()));
 //    }
 
-    public void setZoomBoundCoordinates(ReadableArray coordinates){
-        ReadableMap indexZero = coordinates.getMap(0);
-        setScrollableAreaLimitLongitude(indexZero.getDouble("pWestLatitude"),indexZero.getDouble("pEastLatitude"),0);
-        setScrollableAreaLimitLatitude(indexZero.getDouble("pNorthLatitude"),indexZero.getDouble("pSouthLatitude"),0);
-    }
+//    public void setZoomBoundCoordinates(ReadableArray coordinates){
+//        ReadableMap indexZero = coordinates.getMap(0);
+//        setScrollableAreaLimitLongitude(indexZero.getDouble("pWestLatitude"),indexZero.getDouble("pEastLatitude"),0);
+//        setScrollableAreaLimitLatitude(indexZero.getDouble("pNorthLatitude"),indexZero.getDouble("pSouthLatitude"),0);
+//    }
 
     /*
      * Disabled builtin zoom controls for good, because it does not work
